372e31dbd2b983d74437d3b1d8eed485aaa3e37f
let-lucasOliveira <lucas.nro1000@gmail.com>
2024-09-09 12:22:21 -0300

    Add new files and modules

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import express from "express";
 //Foi instalado a biblioteca @types/express -D (-D significa que será usado somente no desenvolvimento, pois o código será convertido em JS quando subir para o navegador)
 //Foi instalado a biblioteca ts-node -D (Pois o node nativo não consegue ler um código em TypeScript)
+//Foi instalado a biblioteca ts-node-dev -D (Sempre que for realizado uma alteração no arquivo server.ts não será necessário reiniciar o arquivo)
 
 const app = express();
 
 app.get('/users', (request, response) => {
     console.log('Listagem de usuários');
 
-    response.send('Hello World!');
+    response.json([
+        'Lucas',
+        'Paulo',
+        'Barreto',
+        'Robert',
+        'Abraao'
+    ]);
 });
 
 app.listen(3333);
